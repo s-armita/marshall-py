11de2ebed4931e303f9f8085cbb04b37ef13f9d7
Armita S <1yo.armii9@gmail.com>
2025-02-12 14:51:56 -0500

    lesson3.py

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,8 @@
 # Lesson 3
+# input
+titles = input("Enter the number of tiles: ")
+# input() always a string
+tiles = int(tiles)
+calculation = int((tiles ** 0.5) //1)
+
+print("The maximum side length is: {calculation}")
