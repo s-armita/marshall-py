cd53bd96691b534f6fcc063016c7e65653d76ea2
J Park <mrparkonline@gmail.com>
2025-02-14 14:25:57 -0500

    cleaned up lesson

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,6 +1,1 @@
 # Lesson 3
-# Maximum square side length based on input
-tiles = input("Enter the number of tiles: ")
-tiles = int(tiles)
-calculation = int((tiles ** 0.5) // 1)
-print(f"The maximum side length is: {calculation}")
